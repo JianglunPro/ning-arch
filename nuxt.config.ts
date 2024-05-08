ddb01226058bbbbb72e3808a32ff8e7a58d2ea2f
jianglun <jianglun_pro@126.com>
2024-05-08 23:06:09 +0800

    应用配置 runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,19 @@ export default defineNuxtConfig({
 
   // 根目录
   srcDir: 'src',
+
+  // 环境变量
+  // 可以被 .env 文件中的值覆盖
+  // NUXT_PUBLIC_APP_NAME=鲲浪
+  // NUXT_SURREAL_ROOT_USER=root
+  // NUXT_SURREAL_ROOT_PASS=123
+  runtimeConfig: {
+    public: {
+      appName: '宁皓网',
+    },
+    surreal: {
+      rootUser: 'root',
+      rootPass: 'xxx',
+    },
+  },
 });
--- src/app.vue
+++ src/app.vue
@@ -3,3 +3,11 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig();
+
+// 只会打印出 public 中的配置
+// 其他配置会被视为 private 不会被打印, 开发服务器中会自动打印所有配置, 也可以在 nuxt devtool 中查看
+console.log(config);
+</script>
