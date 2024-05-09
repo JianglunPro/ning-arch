46a514081a368afdc6a51f9ea02066b0c0c01da0
jianglun <jianglun_pro@126.com>
2024-05-09 22:56:19 +0800

    设置网页图标

--- src/app.vue
+++ src/app.vue
@@ -12,5 +12,34 @@ useHead({
   titleTemplate(title) {
     return title ? `${title} - ${appName}` : appName;
   },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      type: 'image/png',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+  ],
 });
 </script>
