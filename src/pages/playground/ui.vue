acf8f54d639aff02e628f02ff1200420b23ea54d
jianglun <jianglun_pro@126.com>
2024-05-11 21:32:51 +0800

    尝试一下属性和插槽

--- src/pages/playground/ui.vue
+++ src/pages/playground/ui.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="flex-1 flex flex-col items-center justify-center">
     <div class="px-8 py-4">
-      <UButton>按钮</UButton>
+      <UButton color="amber" variant="outline" size="lg">
+        按钮
+        <template #leading>
+          <UAvatar src="https://avatars.githubusercontent.com/u/35514521?v=4" size="xs" />
+        </template>
+      </UButton>
     </div>
   </div>
 </template>
