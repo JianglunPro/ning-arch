857f49ac6373449cece0cfd666e98e45b6827cda
jianglun <jianglun_pro@126.com>
2024-05-12 19:42:29 +0800

    创建初创～

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>鲲浪</div>
+  <div
+    :class="[
+      'px-4 py-6 text-gray-500 font-normal text-center',
+      'flex-1 flex flex-col items-center justify-center',
+    ]"
+  >
+    🦄 独立开发者训练营
+  </div>
 </template>
